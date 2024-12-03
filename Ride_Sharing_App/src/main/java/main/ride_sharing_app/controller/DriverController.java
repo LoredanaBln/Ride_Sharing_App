2ce33de85de6bbe7c2230ad7c8afb65cf2a96cd9
Loredana Balan <15789abcd@gmail.com>
2024-12-03 12:50:46 +0200

    rebasing

--- Ride_Sharing_App/src/main/java/main/ride_sharing_app/controller/DriverController.java
+++ Ride_Sharing_App/src/main/java/main/ride_sharing_app/controller/DriverController.java
@@ -5,10 +5,7 @@ import main.ride_sharing_app.service.DriverService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/driver")
@@ -18,6 +15,11 @@ public class DriverController {
 
     public DriverController(DriverService driverService) {
         this.driverService = driverService;
+    }
+
+    @PostMapping("/signUp")
+    public ResponseEntity<Driver> signUpDriver(@RequestBody Driver driver) {
+        return ResponseEntity.status(HttpStatus.OK).body(driverService.createDriver(driver));
     }
 
     @GetMapping("/id/{id}")
