4baa1e8a8a9691fa652e0bbf6916a3e19cf8489b
Loredana Balan <15789abcd@gmail.com>
2025-01-07 18:51:25 +0200

    rebasing main

--- Ride_Sharing_UI/src/vite-env.d.ts
+++ Ride_Sharing_UI/src/vite-env.d.ts
@@ -1,1 +1,11 @@
-// <reference model="vite/client" />
+/// <reference types="vite/client" />
+
+declare module '*.png' {
+    const content: string
+    export default content
+}
+
+declare module '*.jpg' {
+    const content: string
+    export default content
+}
--- Ride_Sharing_UI/vite.config.ts
+++ Ride_Sharing_UI/vite.config.ts
@@ -1,21 +1,20 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    host: true
-  },
-  resolve: {
-    alias: {
-      // This handles modules that expect Node.js globals
-      global: {}
-    }
-  },
-  define: {
-    // This adds missing globals that some modules expect
-    global: 'window'
-  }
+    plugins: [react()],
+    server: {
+        port: 5173,
+        host: true
+    },
+    resolve: {
+        alias: {
+            global: 'window'
+        }
+    },
+    define: {
+        global: 'window'
+    },
+    assetsInclude: ['**/*.png', '**/*.jpg']
 })
